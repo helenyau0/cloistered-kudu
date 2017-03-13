9522d3d0374f9ab6c7b924848d6627fa1f45f4eb
Helen Yau <helenyau13@gmail.com>
2017-03-13 16:43:10 -0700

    implmenet add item functionality as well as item strikethrough

--- public/main.js
+++ public/main.js
@@ -1,14 +1,35 @@
+
+
+function strikeThrough() {
+  var checkId = this.id.replace("checkId", "")
+  var newLi = document.getElementById('li' + checkId)
+  if(this.checked) {
+    newLi.style.textDecoration = 'line-through'
+  } else {
+    newLi.style.textDecoration = "none";
+  }
+}
+var totalItems = 0;
+
 function submitButton() {
+  totalItems++
+
   var checkBox = document.createElement('input')
-  checkBox.setAttribute('type', 'checkbox')
+  checkBox.type = 'checkbox'
+  checkBox.id = "checkId" + totalItems
+  checkBox.onclick = strikeThrough
+  checkBox.contentEditable = "false"
 
   var input = document.getElementById('input').value
   document.getElementById('input').value = ""
-  
+
   var li = document.createElement('li')
+  li.id = "li" + totalItems
   var textNode = document.createTextNode(input)
   li.appendChild(checkBox)
   li.appendChild(textNode)
+  li.contentEditable = 'true'
+
   var listItems = document.getElementById('listItems')
   listItems.appendChild(li)
 }
